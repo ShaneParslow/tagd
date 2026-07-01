992d4b5f77923cf6290e9f44c963b511aa00b8c4
Shane Parslow <shaneparslow808@gmail.com>
2026-06-30 19:04:46 -0700

    core: Add TaggerInfo struct

--- tagd-core/src/lib.rs
+++ tagd-core/src/lib.rs
@@ -4,4 +4,12 @@ use serde::{Deserialize, Serialize};
 pub struct TaggerResponse {
     pub tagger: String,
     pub tags: Vec<(String, String)>,
+    pub mtime_at_tag: i64,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TaggerInfo {
+    pub name: String,
+    pub version: String,
+    pub keys: Vec<String>,
 }
