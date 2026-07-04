31b0c7b462fced0d13ff0ff0af00e34afd4f7ebe
Shane Parslow <shaneparslow808@gmail.com>
2026-07-03 17:39:30 -0700

    tagd: Move mpsc init into spawn_event_providers

--- tagd/src/event.rs
+++ tagd/src/event.rs
@@ -1,13 +1,17 @@
 mod fanotify;
 
-use std::path::PathBuf;
+use std::path::{PathBuf};
 use std::sync::mpsc;
 use std::thread;
 
-/// Create threads for all event providers
-pub fn spawn_event_providers(tx: mpsc::Sender<PathBuf>) {
+/// Create threads for all event providers.
+pub fn spawn_event_providers() -> mpsc::Receiver<PathBuf> {
+    let (tx, rx) = mpsc::channel();
+    
     let fa = fanotify::fan_provider_init();
     thread::spawn(
         move || fanotify::fan_provider_exec(fa, tx)
     );
+
+    rx
 }
--- tagd/src/main.rs
+++ tagd/src/main.rs
@@ -5,20 +5,12 @@ mod tagger;
 mod db;
 mod socket;
 
-use std::sync::mpsc;
-
 fn main() {
-    // Event threads to queue channel
-    let (tx, rx) = mpsc::channel();
-
     let taggers = tagger::scan_taggers(); // TODO: proper tagger registry tracking keys for each
 
-    let queue = queue::Queue::new(taggers, rx);
-    
     socket::spawn_socket_listener();
-    event::spawn_event_providers(tx);
     
-
-
+    let rx = event::spawn_event_providers();
+    let queue = queue::Queue::new(taggers, rx);
     queue.run();
 }
